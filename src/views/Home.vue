a77524e9742b876567716e4a9a43d04ba74c556e
felgomoli1994 <felipe.gomes@livup.com.br>
2020-03-08 17:57:11 -0300

    Remove Catalog and Category components

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <fragment>
-    <Catalog v-if="categories" :categories="categories" />
+    <section v-for="category in categories" :key="category.id">
+      <CategoryName>{{ category.name }}</CategoryName>
+      <CategoryDescription>{{ category.description }}</CategoryDescription>
+      <ItemList :items="category.items" />
+    </section>
     <router-link v-if="basket.length" :to="BASKET_PATH">Cesta</router-link>
   </fragment>
 </template>
 
 <script>
-  import Catalog from '@/components/Catalog.vue'
+  import CategoryDescription from '@/components/CategoryDescription.vue'
+  import CategoryName from '@/components/CategoryName.vue'
+  import ItemList from '@/components/ItemList.vue'
   import { BASKET_PATH } from '@/router'
 
   export default {
     name: 'Home',
     components: {
-      Catalog
+      CategoryDescription,
+      CategoryName,
+      ItemList
     },
     data: function() {
       return {
